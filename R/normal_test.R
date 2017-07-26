33bcfab9cc2faea3d3d8f0e5075c3b840431f714
yijin71 <yi.jin71@outlook.com>
2017-07-26 16:01:55 +0100

    plot.normal and normaltest

---
+++ R/normal_test.R
@@ -0,0 +1,21 @@
+#' Bayesian hypothesis testing with Normal distribution
+#'
+#'@param x
+#'@param theta
+#'
+#'@export
+normaltest <- function(x, theta, a, b) {
+  res <-x
+  pos.mean <- res$pos.mean
+  pos.var <- res$pos.var
+  p1 <- abs(pos.mean - theta) / sqrt(pos.var)
+  p2 <- 1 -p1
+  pos.loss1 <- p1 * a
+  pos.loss2 <- p2 * b
+  if (pos.loss1 <=  pos.loss2) {
+    cat(paste("We reject H0 and the expected posterior loss is :", round(pos.loss1,5)))
+  }
+  if (pos.loss1 > pos.loss2) {
+    cat(paste("We accept H0 and the expected posterior loss is :", round(pos.loss2,5)))
+  }
+}
--- R/plot_normal.R
+++ R/plot_normal.R
@@ -11,20 +11,29 @@
 #'
 #'## if mu and sigma  are unknown
 #'exmp2 <- normal(xx,m = 25, s = sqrt(10), a = 4, b = 1)
-#'plot.normal(exmp2)
+#'plot.normal(exmp2, which = "1")
+#'plot.normal(exmp2, which = "2", xlim =range(16:32), ylim =range(0:10))
+#'
 #'@export plot.normal
 plot.normal <- function (x, y, leg_pos = c("topright", "bottomright",
                                            "bottom", "bottomleft",
                                            "left", "topleft",
-                                           "top", "right", "center"),...) {
+                                           "top", "right", "center"), which = "",...) {
 
   if(length(x) > 8) {
+#############################################################################
+# when sigma is unknown
   res <- x
+#############################################################################
+# mu, and mu.prior
   mu <- res$mu
   mu.prior <- res$mu.prior
+#############################################################################
+# tau, tau range and tau.prior
   tau <- res$tau
   tau.prior <-res$tau.prior
   prior <- res$prior
+
   posterior <- res$posterior
 
   x <- cbind(mu, mu, mu)
@@ -49,7 +58,7 @@ plot.normal <- function (x, y, leg_pos = c("topright", "bottomright",
                          title.adj, seg.len, nlevels, levels, labels, labcex, drawlabels,
                          method, vfont, frame.plot, zlim) {
     graphics::matplot(xx, yy, col = col, xlab = xlab, ylab = ylab,
-                      type = type, lty = lty, ...)
+                      type = type, lty = lty...)
   }
   my_legend <- function(pos, lty = 1:3, lwd = 1, col = 1,
                         legend = c("mu.prior","prior", "posterior"), ..., xlab, ylab,
@@ -79,13 +88,15 @@ plot.normal <- function (x, y, leg_pos = c("topright", "bottomright",
                      ...)
   }
 
-  contour <- function(x,y,z, xlab = "mu", ylab="tau",..., legend, fill,
+  contour <- function(x,y,z, xlab = "mu", ylab="tau",
+                      ..., legend, fill,
                       border, angle, density, box.lwd, box.lty, box.col,
                       pt.bg, pt.cex, pt.lwd, xjust, yjust, x.intersp,
                       y.intersp, adj, text.width, text.col, text.font,
                       merge, trace, ncol, horiz, inset, title.col,
-                      title.adj, seg.len,lend, xlim, ylim, add, verbose, type){
-    graphics::contour(x, y, z, xlab = xlab, ylab = ylab,...)
+                      title.adj, seg.len,lend, add, verbose, type){
+    graphics::contour(x, y, z, xlab = xlab, ylab = ylab,
+                      ...)
   }
 
   my_legend3 <- function(pos, lty = 1, lwd = 1, col = 1,
@@ -97,17 +108,25 @@ plot.normal <- function (x, y, leg_pos = c("topright", "bottomright",
                      ...)
   }
 
-  par(mfrow=c(3,1))
+  if(which == "1") {
+
+  par(mfrow=c(2,1))
 
   my_matplot(x, y, ...)
   my_legend(leg_pos, ...)
   my_matplot2(xx, yy, ...)
   my_legend2(leg_pos, ...)
+  }
+
+  if (which == "2") {
+    par(mfrow=c(1,1))
   contour(mu, tau, outer(mu.prior,tau.prior),...)
   my_legend3(leg_pos, ...)
+  }
 
   } else {
-
+#############################################################################
+# when sigma is known
     res <- x
     mu.prior <- res$prior
     posterior <- res$posterior
